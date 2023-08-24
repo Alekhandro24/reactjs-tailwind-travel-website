cdda0ce7a7fdec868f1f2961a970a4451de011e1
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-08-24 12:07:13 +0300

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
         <form className="flex justify-between items-center max-w-[700px] mx-auto w-full border p-1 rounded-md text-black bg-gray-100/90">
           <div>
             <input
-              className="bg-transparent w-[300px] sm:w-[400px] font-[Poppins] focus:outline-none"
+              className="bg-transparent w-[290px] sm:w-[400px] font-[Poppins] focus:outline-none"
               type="text"
               placeholder="Search Destinations"
             />
